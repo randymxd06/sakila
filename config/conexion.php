73a580ea7ea8c1a97bb287daec31149c440da432
randymxd06 <randym0624@gmail.com>
2020-07-13 22:12:31 -0400

    ahora puedo agregar la base de datos a 000webhost

--- config/conexion.php
+++ config/conexion.php
@@ -1,14 +1,30 @@
 <?php
 
+//Valores de mi base de datos de 000webhost//
+if($_SERVER['SERVER_NAME'] == 'sakila0624.000webhostapp.com') {
+    $host = "localhost";
+    $dbname = "id14138384_sakila";
+    $usuario = "id14138384_root";
+    $password = "j3NVvSi{6U6\mFqA";
+}
+
+//Valores de mi base de datos local (mi pc)//
+$host = "localhost";
+$dbname = "sakila";
 $usuario = "root";
 $password = "rayquaza51";
 
-$errores = [];
+$ajustes = [
+    19 => 2
+];
+
+//$errores = [];
 
 try{
-    $conexion = new PDO('mysql:host=localhost;dbname=sakila', $usuario, $password);
+    $conexion = new PDO("mysql:host={$host};dbname={$dbname}", $usuario, $password, $ajustes);
 }catch(PDOException $exception){
-    array_push($errores, $exception -> getMessage());
+    throw new Exception("Hubo un error al conectarnos a la base de datos: {$exception}");
+    //array_push($errores, $exception -> getMessage());
 }
 
 
--- modelos/modelo_pelicula.php
+++ modelos/modelo_pelicula.php
@@ -8,7 +8,7 @@ function obtenerPeliculas($conexion){
     $sql = "SELECT fi.film_id, fi.title, fi.description, fi.release_year, lan.name AS Idioma, lan2.name AS Idioma_Original, fi.rental_duration, fi.rental_rate, fi.length, fi.replacement_cost, fi.rating, fi.special_features 
             FROM film AS fi
             INNER JOIN language AS lan ON lan.language_id = fi.language_id
-            INNER JOIN language AS lan2 ON lan2.language_id = fi.language_id";
+            LEFT JOIN language AS lan2 ON lan2.language_id = fi.original_language_id";
 
     return $conexion->query($sql)->fetchAll();
 
