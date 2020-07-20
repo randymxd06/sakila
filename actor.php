354c7624a6f1e8db639f9512c1a6a22733ca8660
randymxd06 <randym0624@gmail.com>
2020-07-19 21:51:46 -0400

    nada :v

--- actor.php
+++ actor.php
@@ -8,33 +8,33 @@ require_once "modelos/modelo_actor.php";
 $actores = obtenerActores($conexion);
 
 //Declarar variables//
-$nombreActor = $_GET['inputNombreActor'] ?? "";
-$apellidoActor = $_GET['inputApellidoActor'] ?? "";
-$btnGuardadDatos = $_GET['btnGuardarDatos'] ?? "";
+$nombreActor = $_POST['inputNombreActor'] ?? "";
+$apellidoActor = $_POST['inputApellidoActor'] ?? "";
+$btnGuardadDatos = $_POST['btnGuardarDatos'] ?? "";
 
 //Asegurarnos de que el usuario haya hecho click en el boton Guardar Datos//
-if(isset($_GET['btnGuardarDatos'])){
-    //Codigo para guardar en la base de datos//
-    echo "Se van a guardar los datos...";
+if(isset($_POST['btnGuardarDatos'])){
 
     //Validar los datos//
     // TODO
 
-    /*$datos = [
-      'inputNombreActor' => $nombreActor,
-      'inputApellidoActor' => $apellidoActor
-    ];*/
+    if(empty($nombreActor)){
+        throw new Error("El nombre del actor esta vacio, porfavor llenarlo");
+    }
+    if(empty($apellidoActor)){
+        throw new Error("El apellido del actor esta vacio, porfavor llenarlo");
+    }
+    
+        $datos = [
+            'inputNombreActor' => $nombreActor,
+            'inputApellidoActor' => $apellidoActor
+        ];
 
-    $datos = compact('nombreActor', 'apellidoActor');
+        //$datos = compact('nombreActor', 'apellidoActor');
 
-    $insertando = insertarActores($conexion, $datos);
+        $insertando = insertarActores($conexion, $datos);
 
-    if($insertando)
-        echo "Datos insertados correctamente";
-    else
-        echo "No se pudo insertar los datos";
-
-}
+}//Fin del if//
 
 //Incluir la vista//
 include_once "vistas/vista_actor.php";
